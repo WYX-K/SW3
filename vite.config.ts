69254a785bd3e8527130d7a2138a1c009849e412
WYX-K <1135613552@qq.com>
2022-04-04 15:11:20 +0800

    init

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { createStyleImportPlugin } from 'vite-plugin-style-import';
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    createStyleImportPlugin({
+      libs: [
+        {
+          libraryName: '@arco-design/web-vue',
+          esModule: true,
+          resolveStyle: (name) => {
+            // css
+            return `@arco-design/web-vue/es/${name}/style/css.js`
+            // less
+            return `@arco-design/web-vue/es/${name}/style/index.js`
+          },
+        }
+      ]
+    })
+  ]
+})
